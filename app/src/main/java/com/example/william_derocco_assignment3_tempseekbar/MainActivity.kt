0889bd7e4b70f39d26f1193a2135731107ed1abd
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-02-12 16:00:58 -0500

    added code to update the fahrenheit seekbar/value

--- app/src/main/java/com/example/william_derocco_assignment3_tempseekbar/MainActivity.kt
+++ app/src/main/java/com/example/william_derocco_assignment3_tempseekbar/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.william_derocco_assignment3_tempseekbar
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Find the SeekBar for Fahrenheit by its ID
+        val fahrenheitSeekBar = findViewById<SeekBar>(R.id.fahrenheitSeekBar)
+
+        // Set an OnSeekBarChangeListener to the Fahrenheit SeekBar
+        fahrenheitSeekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                // Update the TextView displaying the Fahrenheit value
+                val valueTextView = findViewById<TextView>(R.id.valueFahrenheit)
+                valueTextView.text = progress.toString()
+
+
+            }
+
+            // We don't need these two methods for our example, but they must be implemented
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // not required i guess?
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // not required i guess?
+            }
+        })
     }
 }
