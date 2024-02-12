2cac9ba13c0ef78fbea9a86cb3d074b09fc2cd12
William De Rocco <93288641+wderocco8@users.noreply.github.com>
2024-02-12 15:52:50 -0500

    Initial commit + added UI for seekbards

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "William_DeRocco_Assignment3_TempSeekbar"
+include(":app")
+ 
